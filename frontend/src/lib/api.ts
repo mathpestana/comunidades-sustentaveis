1792276ea6aad24ba34a12c7e3e59ec3d7fcf6b6
Matheus Pestana <168146077+mathpestana@users.noreply.github.com>
2025-07-12 18:42:01 -0300

    Hosted API and configured base Axios setup with environment variables

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",  //temporario, necessario hospedar o back e colocar ele no env e chamar aqui
+  baseURL: API_BASE_URL,  //temporario, necessario hospedar o back e colocar ele no env e chamar aqui
   withCredentials: true, 
 });
+
+
+//adicionar os tipos das respostas da api 
+
+
+
+//funcoes para consumir a api
